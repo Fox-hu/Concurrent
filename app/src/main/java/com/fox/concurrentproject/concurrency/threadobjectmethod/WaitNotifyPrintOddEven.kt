e25c7cf1d999181873297f2776fa93618975972b
hujia <hujia0523@gmail.com>
2022-07-30 20:22:27 +0800

    fix using wait/notify to print odd even

--- app/src/main/java/com/fox/concurrentproject/concurrency/threadobjectmethod/WaitNotifyPrintOddEven.kt
+++ app/src/main/java/com/fox/concurrentproject/concurrency/threadobjectmethod/WaitNotifyPrintOddEven.kt
@@ -5,22 +5,29 @@ package com.fox.concurrentproject.concurrency.threadobjectmethod
  * @Date 2022/5/4 11:25
  * 两个线程交替打印0~100的奇偶数 用wait和notify来实现
  */
-fun main() {
-    val runnable = Runnable {
-        val lock = Object()
-        var count = 0
+
+class OddEvenRunnable: Runnable {
+    var count = 0
+    private val oddEvenLock = Object()
+
+    override fun run() {
         while (count <= 100) {
-            synchronized(lock) {
+            synchronized(oddEvenLock) {
                 //1、拿到锁我们就打印
                 println("${Thread.currentThread().name} : ${count++}")
                 //2、打印完 唤醒其他线程 自己就休眠
-                lock.notify()
-                if (count <= 100) {
-                    lock.wait()
-                }
+                oddEvenLock.notify()
+                //这里还需要判断一次count <= 100 是因为之前的count++了
+                if(count <= 100)
+                    oddEvenLock.wait()
             }
         }
     }
+}
+
+
+fun main() {
+    val runnable = OddEvenRunnable()
     Thread(runnable, "偶数线程").start()
     Thread(runnable, "奇数线程").start()
 }
